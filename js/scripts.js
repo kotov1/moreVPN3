64d7bfd4986330dd7c98f65bb3a2b8e91a683668
Petr Petukhov <kotov0991@gmail.com>
2018-04-15 23:47:00 +0300

    mob

--- js/scripts.js
+++ js/scripts.js
@@ -41,20 +41,6 @@ $(function() {
 		});
 	});
 
-	function setEqualHeight(columns){
-	var tallestcolumn = 0;
-	columns.each(function(){
-		currentHeight = $(this).height();
-		if(currentHeight > tallestcolumn){
-			tallestcolumn = currentHeight;
-		}
-	});
-	columns.height(tallestcolumn);
-	}
-	$(document).ready(function() {
-	setEqualHeight($(".endorsment-post"));
-	});
-
 
 	
 	$(".error").hide();
